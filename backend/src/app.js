e7b705c84dba239cdbe798adfd86420a1073093d
fabricio <vigofabricio@gmail.com>
2023-02-06 14:30:52 -0300

    routes and request handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import productsRoutes from './routes/products';
+import productsRoutes2 from './routes/products2';
+import clientRoute from './routes/clients';
+import orderRoute from './routes/orders';
 
 const app = express();
 
 app.use(express.json());
 app.use('/', productsRoutes);
+app.use('/', productsRoutes2);
+app.use('/', clientRoute);
+app.use('/', orderRoute);
+
+
 
 
 
--- backend/src/database.js
+++ backend/src/database.js
@@ -14,11 +14,3 @@ connection.connect((err) => {
   }
   console.log('Connected to the MySQL server.');
 });
-/* import mysql from 'mysql2/promise';
-import { config } from './config';
-
-export const connect = async () => {
-   return await mysql.createConnection(config);
-};
- 
-connect();  */
